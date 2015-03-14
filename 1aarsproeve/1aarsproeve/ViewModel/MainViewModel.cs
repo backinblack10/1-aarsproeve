33e62947fba64e69d496d8d7a747e5874e155078
Daniel <danielwinther@hotmail.dk>
2015-03-14 11:42:16 +0100

    up

--- 1aarsproeve/1aarsproeve/ViewModel/MainViewModel.cs
+++ 1aarsproeve/1aarsproeve/ViewModel/MainViewModel.cs
@@ -17,8 +17,6 @@ namespace _1aarsproeve.ViewModel
 {
     class MainViewModel
     {
-        public string Ugenummer { get; set; }
-
         public Brush MandagFarve { get; set; }
         public Brush TirsdagFarve { get; set; }
         public Brush OnsdagFarve { get; set; }
@@ -26,6 +24,8 @@ namespace _1aarsproeve.ViewModel
         public Brush FredagFarve { get; set; }
         public Brush LoerdagFarve { get; set; }
         public Brush SoendagFarve { get; set; }
+
+        public int Ugenummer { get; set; }
 
         public string Mandag { get; set; }
         public string Tirsdag { get; set; }
@@ -38,8 +38,75 @@ namespace _1aarsproeve.ViewModel
 
         public MainViewModel()
         {
-            SolidColorBrush brush = new SolidColorBrush(Color.FromArgb(100, 255, 255, 255));
-            SolidColorBrush brushOriginal = new SolidColorBrush(Color.FromArgb(100, 162, 218, 255));
+            NuvaerendeUgedag(new SolidColorBrush(Color.FromArgb(100, 255, 255, 255)), new SolidColorBrush(Color.FromArgb(100, 162, 218, 255)));
+
+            FindUgenummer("da-DK");
+
+            Mandag = FoersteDagPaaUge(Ugenummer).ToString("dd/MM-yyyy");
+            Tirsdag = FoersteDagPaaUge(Ugenummer).AddDays(1).ToString("dd/MM-yyyy");
+            Onsdag = FoersteDagPaaUge(Ugenummer).AddDays(2).ToString("dd/MM-yyyy");
+            Torsdag = FoersteDagPaaUge(Ugenummer).AddDays(3).ToString("dd/MM-yyyy");
+            Fredag = FoersteDagPaaUge(Ugenummer).AddDays(4).ToString("dd/MM-yyyy");
+            Loerdag = FoersteDagPaaUge(Ugenummer).AddDays(5).ToString("dd/MM-yyyy");
+            Soendag = FoersteDagPaaUge(Ugenummer).AddDays(6).ToString("dd/MM-yyyy");
+
+
+            #region Random generet data
+            UgedageCollection = new ObservableCollection<Ugedage>()
+            {
+                new Ugedage {Ugedag = "Mandag", AnsatteListe = new List<Ansatte>()},
+                new Ugedage {Ugedag = "Tirsdag", AnsatteListe = new List<Ansatte>()},
+                new Ugedage {Ugedag = "Onsdag", AnsatteListe = new List<Ansatte>()},
+                new Ugedage {Ugedag = "Torsdag", AnsatteListe = new List<Ansatte>()},
+                new Ugedage {Ugedag = "Fredag", AnsatteListe = new List<Ansatte>()},
+                new Ugedage {Ugedag = "Lørdag", AnsatteListe = new List<Ansatte>()},
+                new Ugedage {Ugedag = "Søndag", AnsatteListe = new List<Ansatte>()},
+            };
+            Random r = new Random();
+            for (int i = 0; i < UgedageCollection.Count/2; i++)
+            {
+                UgedageCollection[r.Next(0, UgedageCollection.Count)].AnsatteListe.Add(new Ansatte
+                {
+                    Navn = "Daniel Winther",
+                    Tidspunkt = "16:00 - 22:30"
+                });
+                UgedageCollection[r.Next(0, UgedageCollection.Count)].AnsatteListe.Add(new Ansatte
+                {
+                    Navn = "Benjamin Jensen",
+                    Tidspunkt = "7:00 - 16:50"
+                });
+                UgedageCollection[r.Next(0, UgedageCollection.Count)].AnsatteListe.Add(new Ansatte
+                {
+                    Navn = "Jari Larsen",
+                    Tidspunkt = "8:30 - 17:30"
+                });
+                UgedageCollection[r.Next(0, UgedageCollection.Count)].AnsatteListe.Add(new Ansatte
+                {
+                    Navn = "Jacob Balling",
+                    Tidspunkt = "6:00 - 14:20"
+                });
+                UgedageCollection[r.Next(0, UgedageCollection.Count)].AnsatteListe.Add(new Ansatte
+                {
+                    Navn = "Peter Hansen",
+                    Tidspunkt = "10:00 - 18:00"
+                });
+                UgedageCollection[r.Next(0, UgedageCollection.Count)].AnsatteListe.Add(new Ansatte
+                {
+                    Navn = "Christian Johansen",
+                    Tidspunkt = "11:00 - 15:30"
+                });
+                UgedageCollection[r.Next(0, UgedageCollection.Count)].AnsatteListe.Add(new Ansatte
+                {
+                    Navn = "Ubemandet",
+                    Tidspunkt = "8:00 - 21:40"
+                });
+            }
+            #endregion
+        }
+
+
+        public void NuvaerendeUgedag(SolidColorBrush brush, SolidColorBrush brushOriginal)
+        {
             if (MandagFarve == null || TirsdagFarve == null || OnsdagFarve == null || TorsdagFarve == null || FredagFarve == null || LoerdagFarve == null || SoendagFarve == null)
             {
                 MandagFarve = brushOriginal;
@@ -80,43 +147,13 @@ namespace _1aarsproeve.ViewModel
                     SoendagFarve = brush;
                     break;
             }
-            var kultur = CultureInfo.DefaultThreadCurrentCulture = new CultureInfo("da-DK");
-            int ugenummer = kultur.Calendar.GetWeekOfYear(DateTime.Today, DateTimeFormatInfo.GetInstance(kultur).CalendarWeekRule, DateTimeFormatInfo.GetInstance(kultur).FirstDayOfWeek);
-            Ugenummer = "Uge: " + ugenummer;
-
-            int aar = DateTime.Today.Year;
-            Mandag = FoersteDagPaaUge(ugenummer).ToString("dd/MM-yyyy");
-            Tirsdag = FoersteDagPaaUge(ugenummer).AddDays(1).ToString("dd/MM-yyyy");
-            Onsdag = FoersteDagPaaUge(ugenummer).AddDays(2).ToString("dd/MM-yyyy");
-            Torsdag = FoersteDagPaaUge(ugenummer).AddDays(3).ToString("dd/MM-yyyy");
-            Fredag = FoersteDagPaaUge(ugenummer).AddDays(4).ToString("dd/MM-yyyy");
-            Loerdag = FoersteDagPaaUge(ugenummer).AddDays(5).ToString("dd/MM-yyyy");
-            Soendag = FoersteDagPaaUge(ugenummer).AddDays(6).ToString("dd/MM-yyyy");
-
-            UgedageCollection = new ObservableCollection<Ugedage>()
-            {
-                new Ugedage { Ugedag = "Mandag", AnsatteListe = new List<Ansatte>() },
-                new Ugedage { Ugedag = "Tirsdag", AnsatteListe = new List<Ansatte>() },
-                new Ugedage { Ugedag = "Onsdag", AnsatteListe = new List<Ansatte>() },
-                new Ugedage { Ugedag = "Torsdag", AnsatteListe = new List<Ansatte>() },
-                new Ugedage { Ugedag = "Fredag", AnsatteListe = new List<Ansatte>() },
-                new Ugedage { Ugedag = "Lørdag", AnsatteListe = new List<Ansatte>() },
-                new Ugedage { Ugedag = "Søndag", AnsatteListe = new List<Ansatte>() },
-            };
-
-            Random r = new Random();
-            for (int i = 0; i < UgedageCollection.Count / 2; i++)
-            {
-                UgedageCollection[r.Next(0, UgedageCollection.Count)].AnsatteListe.Add(new Ansatte { Navn = "Daniel Winther", Tidspunkt = "16:00 - 22:30" });
-                UgedageCollection[r.Next(0, UgedageCollection.Count)].AnsatteListe.Add(new Ansatte { Navn = "Benjamin Jensen", Tidspunkt = "7:00 - 16:50" });
-                UgedageCollection[r.Next(0, UgedageCollection.Count)].AnsatteListe.Add(new Ansatte { Navn = "Jari Larsen", Tidspunkt = "8:30 - 17:30" });
-                UgedageCollection[r.Next(0, UgedageCollection.Count)].AnsatteListe.Add(new Ansatte { Navn = "Jacob Balling", Tidspunkt = "6:00 - 14:20" });
-                UgedageCollection[r.Next(0, UgedageCollection.Count)].AnsatteListe.Add(new Ansatte { Navn = "Peter Hansen", Tidspunkt = "10:00 - 18:00" });
-                UgedageCollection[r.Next(0, UgedageCollection.Count)].AnsatteListe.Add(new Ansatte { Navn = "Christian Johansen", Tidspunkt = "11:00 - 15:30" });
-                UgedageCollection[r.Next(0, UgedageCollection.Count)].AnsatteListe.Add(new Ansatte { Navn = "Ubemandet", Tidspunkt = "8:00 - 21:40" });
-            }
         }
-
+        public void FindUgenummer(string kulturInfo)
+        {
+            var kultur = CultureInfo.DefaultThreadCurrentCulture = new CultureInfo(kulturInfo);
+            int ugenummer = kultur.Calendar.GetWeekOfYear(DateTime.Today, DateTimeFormatInfo.GetInstance(kultur).CalendarWeekRule, DateTimeFormatInfo.GetInstance(kultur).FirstDayOfWeek);
+            Ugenummer = ugenummer;
+        }
         public static DateTime FoersteDagPaaUge(int weekOfYear)
         {
             DateTime jan1 = new DateTime(DateTime.Today.Year, 1, 1);
@@ -136,14 +173,17 @@ namespace _1aarsproeve.ViewModel
         }
     }
 
-    class Ugedage
+    #region Forsøgsklasser
+    internal class Ugedage
     {
         public string Ugedag { get; set; }
         public List<Ansatte> AnsatteListe { get; set; }
     }
-    class Ansatte
+
+    internal class Ansatte
     {
         public string Navn { get; set; }
         public string Tidspunkt { get; set; }
     }
+    #endregion
 }
