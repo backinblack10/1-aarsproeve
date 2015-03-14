b72addd2723e9f0e23812f9cf5783203398e8fa9
Daniel <danielwinther@hotmail.dk>
2015-03-14 15:35:07 +0100

    date

--- 1aarsproeve/1aarsproeve/ViewModel/MainViewModel.cs
+++ 1aarsproeve/1aarsproeve/ViewModel/MainViewModel.cs
@@ -11,8 +11,13 @@ namespace _1aarsproeve.ViewModel
 {
     class MainViewModel
     {
+        public string Ugenummer { get; set; }
         public MainViewModel()
         {
+            var kultur = CultureInfo.DefaultThreadCurrentCulture = new CultureInfo("da-DK");
+            int ugeNummer = kultur.Calendar.GetWeekOfYear(DateTime.Today, DateTimeFormatInfo.GetInstance(kultur).CalendarWeekRule, DateTimeFormatInfo.GetInstance(kultur).FirstDayOfWeek);
+
+            Ugenummer = "Uge: " + ugeNummer;
         }
     }
 }
