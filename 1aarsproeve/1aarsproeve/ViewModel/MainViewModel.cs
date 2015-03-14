740435bc11e1abff29bba02eca8002ada93e5ce6
Daniel <danielwinther@hotmail.dk>
2015-03-14 17:43:20 +0100

    ws

--- 1aarsproeve/1aarsproeve/ViewModel/MainViewModel.cs
+++ 1aarsproeve/1aarsproeve/ViewModel/MainViewModel.cs
@@ -13,6 +13,7 @@ namespace _1aarsproeve.ViewModel
     class MainViewModel
     {
         public string Ugenummer { get; set; }
+        public DateTime Day { get; set; }
         public ObservableCollection<Ansatte> AnsatteCollection { get; set; }
         public MainViewModel()
         {
@@ -20,21 +21,22 @@ namespace _1aarsproeve.ViewModel
             int ugeNummer = kultur.Calendar.GetWeekOfYear(DateTime.Today, DateTimeFormatInfo.GetInstance(kultur).CalendarWeekRule, DateTimeFormatInfo.GetInstance(kultur).FirstDayOfWeek);
             Ugenummer = "Uge: " + ugeNummer;
 
+
+
             AnsatteCollection = new ObservableCollection<Ansatte>()
             {
-                new Ansatte {Navn = "Daniel Winther", Tid = "16:00 - 22:30"},
-                new Ansatte {Navn = "Benjamin Jensen", Tid = "7:00 - 16:50"},
-                new Ansatte {Navn = "Jari Larsen", Tid = "8:30 - 17:30"},
-                new Ansatte {Navn = "Jacob Balling", Tid = "6:00 - 14:20"},
-                new Ansatte {Navn = "Peter Hansen", Tid = "10:00 - 18:00"},
-                new Ansatte {Navn = "Mustafa Johansen", Tid = "11:00 - 15:30"}
+                new Ansatte {Navn = "Daniel Winther", Tidspunkt = "16:00 - 22:30"},
+                new Ansatte {Navn = "Benjamin Jensen", Tidspunkt = "7:00 - 16:50"},
+                new Ansatte {Navn = "Jari Larsen", Tidspunkt = "8:30 - 17:30"},
+                new Ansatte {Navn = "Jacob Balling", Tidspunkt = "6:00 - 14:20"},
+                new Ansatte {Navn = "Peter Hansen", Tidspunkt = "10:00 - 18:00"},
+                new Ansatte {Navn = "Mustafa Johansen", Tidspunkt = "11:00 - 15:30"}
             };
         }
     }
-
     class Ansatte
     {
         public string Navn { get; set; }
-        public string Tid { get; set; }
+        public string Tidspunkt { get; set; }
     }
 }
