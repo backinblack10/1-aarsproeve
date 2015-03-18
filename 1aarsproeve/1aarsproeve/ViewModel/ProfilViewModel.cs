8af258242964bfbf7e397ea44d3042afae0ff7d3
Daniel <danielwinther@hotmail.dk>
2015-03-18 19:40:58 +0100

    colors

---
+++ 1aarsproeve/1aarsproeve/ViewModel/ProfilViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Storage;
+
+namespace _1aarsproeve.ViewModel
+{
+    class ProfilViewModel
+    {
+        public ApplicationDataContainer Setting { get; set; }
+        public string Brugernavn { get; set; }
+
+        public ProfilViewModel()
+        {
+            Setting = ApplicationData.Current.LocalSettings;
+            Brugernavn = (string)Setting.Values["Brugernavn"];
+        }
+    }
+}
